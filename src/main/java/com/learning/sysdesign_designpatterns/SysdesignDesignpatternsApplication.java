607a107b227866420b6de677b7348353fff47cee
SiddharthLucky <siddharth.luckyreddy@gmail.com>
2024-09-24 10:53:51 +0530

    Initial commit

---
+++ src/main/java/com/learning/sysdesign_designpatterns/SysdesignDesignpatternsApplication.java
@@ -0,0 +1,13 @@
+package com.learning.sysdesign_designpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysdesignDesignpatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysdesignDesignpatternsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/sysdesign_designpatterns/SysdesignDesignpatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.sysdesign_designpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysdesignDesignpatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
